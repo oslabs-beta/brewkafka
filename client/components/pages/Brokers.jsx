f868aa99c722ae632ae5cbf4663c7c47d5cfd9d3
Robert <Brobertforrester@gmail.com>
2023-11-14 16:31:42 -0500

    Make minor styling edits: box shadow to sidebar and header, import google fonts into index.html, change fonts of header and sidebar, add spacing to buttons on sidebar.

--- client/components/pages/Brokers.jsx
+++ client/components/pages/Brokers.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 export function Brokers () {
   return (
     <>
-      <h1>This is the Broker page</h1>
-      <p>this is a p element</p>
+      <h1>Welcome to Brokers</h1>
     </>
   )
 }
