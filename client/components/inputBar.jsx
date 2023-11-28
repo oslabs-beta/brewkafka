b6159fd6da7de38ab515a2c9f5937fab1107388c
lomeintheory <andrewlo957@gmail.com>
2023-11-28 12:43:57 -0800

    updated input bar

--- client/components/inputBar.jsx
+++ client/components/inputBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import KafkaService from '../services/KafkaService';
 
 const InputBar = () => {
@@ -9,23 +9,27 @@ const InputBar = () => {
     setInputValue(e.target.value);
   };
 
-    //handle submit
-    const handleSubmit = () => {
-        console.log('Submitted:', inputValue);
+  // handle submit
+  const handleSubmit = async () => {
+    console.log('Submitted: ', inputValue);
+    try {
+      await KafkaService.addKafkaServer(inputValue);
+      console.log('Updated server successfully');
+      await KafkaService.reloadPrometheusConfig();
+      console.log('Prometheus reloaded');
+    } catch (error) {
+      console.log('Error with Kafka config updating: ' + error);
     }
-    return (
-      <div className="input-bar-container">
-        <label>
-            Connect to your Kafka cluster   
-            <input
-            type="text"
-            value={inputValue}
-            onChange={handleChange}
-            />
-        </label>
-        <button onClick={handleSubmit}>Connect!</button>
+  };
+  return (
+    <div className='input-bar-container'>
+      <label>
+        Connect to your Kafka cluster
+        <input type='text' value={inputValue} onChange={handleChange} />
+      </label>
+      <button onClick={handleSubmit}>Connect!</button>
     </div>
-    );
+  );
 };
 
 export default InputBar;
