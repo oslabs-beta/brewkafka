06f75dc6f9bd98b46467f3c8c9005d3ecca7b418
lomeintheory <andrewlo957@gmail.com>
2023-11-29 21:20:05 -0800

    testing out dashboards

--- client/components/pages/Brokers.jsx
+++ client/components/pages/Brokers.jsx
@@ -5,8 +5,8 @@ export function Brokers() {
   return (
     <>
     <Dashboards/>
-      <h1>Brokers Dashboard</h1>
-      <object
+      <h1 id="dashboardHeader">Brokers Dashboard</h1>
+      {/* <object
         data='http://localhost:3000/d-solo/f79e9d99-336c-4e9a-8187-2fe4c1f257fe/active-broker-count?orgId=1&from=now-24h&to=now&panelId=1'
         width='450'
         height='200'
@@ -20,7 +20,19 @@ export function Brokers() {
         data='http://localhost:3000/d-solo/e74a34bc-bc0f-4af6-9c6c-f503eacfa7e4/broker-bytes-in-per-sec?orgId=1&from=now-24h&to=now&panelId=1'
         width='450'
         height='200'
-        frameborder='0'></object>
+        frameborder='0'></object> */}
+         <h1>Active broker count -- docker ver.</h1>
+      <iframe
+        src='http://localhost:3001/d-solo/c815566a-1a0f-4bea-a96b-df57bf18c720/docker-dashboard?orgId=1&panelId=1'
+        width='450'
+        height='200'
+        frameborder='0'></iframe>
+      <h1>Broker bytes in per sec - docker iframe</h1>
+      <iframe
+        src='http://localhost:3001/d-solo/c815566a-1a0f-4bea-a96b-df57bf18c720/docker-dashboard?orgId=1&panelId=9'
+        width='450'
+        height='200'
+        frameborder='0'></iframe>
     </>
   );
 }
--- client/components/pages/Topics.jsx
+++ client/components/pages/Topics.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dashboards from '../Dashboards';
 
 export function Topics() {
   return (
     <>
-      <h1>Topics & Messages Dashboard</h1>
-      <object
+    <Dashboards/>
+      <h1 id="dashboardHeader">Topics & Messages Dashboard</h1>
+      {/* <object
         class='topic-count '
         data='http://localhost:3000/d-solo/fcab298e-8a43-4a92-a740-f11d2e992881/topic-count?orgId=1&from=now-24h&to=now&panelId=1'
         width='450'
@@ -21,7 +23,25 @@ export function Topics() {
         data='http://localhost:3000/d-solo/dd3d2248-cb4f-495f-abe5-331407713c01/message-in-per-sec?orgId=1&from=now-24h&to=now&panelId=1'
         width='450'
         height='200'
-        frameborder='0'></object>
+        frameborder='0'></object> */}
+        <h1>Topic-count iframe</h1>
+      <iframe
+        src='http://localhost:3001/d-solo/c815566a-1a0f-4bea-a96b-df57bf18c720/docker-dashboard?orgId=1&panelId=6'
+        width='450'
+        height='200'
+        frameborder='0'></iframe>
+        <h1>Topic-bytes per sec iframe</h1>
+      <iframe
+        src='http://localhost:3001/d-solo/c815566a-1a0f-4bea-a96b-df57bf18c720/docker-dashboard?orgId=1&panelId=7'
+        width='450'
+        height='200'
+        frameborder='0'></iframe>
+        <h1>Messages in per sec iframe</h1>
+      <iframe
+        src='http://localhost:3001/d-solo/c815566a-1a0f-4bea-a96b-df57bf18c720/docker-dashboard?orgId=1&panelId=8'
+        width='450'
+        height='200'
+        frameborder='0'></iframe>
     </>
   );
 }
